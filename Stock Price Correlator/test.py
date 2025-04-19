e79fc745fb699f7121be98a004857a987122d8f8
Vikranth3140 <vikranth22570@iiitd.ac.in>
2025-04-19 22:55:49 +0530

    us tech stocks 1 year

--- Stock Price Correlator/test.py
+++ Stock Price Correlator/test.py
@@ -1,5 +1,31 @@
 import yfinance as yf
+import pandas as pd
 
-# Fetch historical data for Apple
-data = yf.download("AAPL", start="2020-01-01", end="2021-01-01")
-print(data.head())
+# US Tech Stiocks
+tech_tickers = [
+    "AAPL",   # Apple
+    "MSFT",   # Microsoft
+    "GOOGL",  # Alphabet (Class A)
+    "GOOG",   # Alphabet (Class C)
+    "AMZN",   # Amazon
+    "META",   # Meta Platforms
+    "TSLA",   # Tesla
+    "NVDA",   # Nvidia
+    "ORCL",   # Oracle
+    "INTC",   # Intel
+    "CRM",    # Salesforce
+    "ADBE",   # Adobe
+    "CSCO",   # Cisco
+    "AMD",    # AMD
+    "IBM",    # IBM
+]
+
+
+# Fetch data
+data = yf.download(tech_tickers, start="2024-04-01", end="2025-04-01", group_by='ticker', auto_adjust=True)
+
+# Get closing prices
+closing_prices = pd.DataFrame({ticker: data[ticker]['Close'] for ticker in tech_tickers})
+print(closing_prices.head())
+
+closing_prices.to_csv("us_tech_stock_prices.csv")
